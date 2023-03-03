6e9d7fb24e3fcd6e5bc4ebfb79b9f92eda234178
tanushree gupta <tanushree.gupta@innoraft.com>
2023-03-03 12:30:34 +0530

    FT2023-72-added styles

--- Php-Task7/login.php
+++ Php-Task7/login.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    <form action="welcome.php" method="post">  <!--form created-->
-        Username: <input type="text" name="username" required><br> <!--username input field-->
-        Password: <input type="password" name="password" required><br> <!--Password input field-->
-        <input type="submit" value="Login" name="login">  <!--Login -->
-    </form>
-    <?php
-    session_start();  //session started
-    if(isset($_SESSION['user_name'])){
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $user_name = $_POST["username"];  //storing value 
-    $pass_word = $_POST["password"];   
-    $_SESSION['user_name']= $user_name;  //session variable
-    } 
+  <section class="container">
+  <form action="welcome.php" method="post" class="form">  <!--form created-->
+    Username: <input type="text" name="username" required><br> <!--username input field-->
+    Password: <input type="password" name="password" required><br> <!--Password input field-->
+    <input type="submit" value="Login" name="login">  <!--Login -->
+  </form>
+  </section>
+  <?php
+  session_start();  //session started
+  if(isset($_SESSION['user_name'])){
+  if($_SERVER["REQUEST_METHOD"]=="POST"){
+  $user_name = $_POST["username"];  //storing value 
+  $pass_word = $_POST["password"];   
+  $_SESSION['user_name']= $user_name;  //session variable
+  } 
 }
-?>
-    
+?>    
 </body>
 </html>
--- Php-Task7/welcome.php
+++ Php-Task7/welcome.php
@@ -1,20 +1,21 @@
 <div class="tasks"> 
-    <ul>  <!--list created-->
-        <li><a href="/Php-Task7/Php-Task1/index.php">Task1</a></li>  <!--Task1-->
-        <li><a href="/Php-Task7/Php-Task2/index.php">Task2</a></li>  <!--Task2-->
-        <li><a href="/Php-Task7/Php-Task3/index.php">Task3</a></li>  <!--Task3-->
-        <li><a href="/Php-Task7/Php-Task4/index.php">Task4</a></li>  <!--Task4-->
-        <li><a href="/Php-Task7/Php-Task5/index.php">Task5</a></li>  <!--Task5-->
-        <li><a href="/Php-Task7/Php-Task6/index.php">Task6</a></li>  <!--Task6-->
-        <form action="login.php" method = "POST">
-        <input type="submit" value="Logout" name="logout">   <!--Logout option-->
-        </form>
-    </ul>
+  <ul>  <!--list created-->
+    <li><a href="/Php-Task7/Php-Task1/index.php">Task1</a></li>  <!--Task1-->
+    <li><a href="/Php-Task7/Php-Task2/index.php">Task2</a></li>  <!--Task2-->
+    <li><a href="/Php-Task7/Php-Task3/index.php">Task3</a></li>  <!--Task3-->
+    <li><a href="/Php-Task7/Php-Task4/index.php">Task4</a></li>  <!--Task4-->
+    <li><a href="/Php-Task7/Php-Task5/index.php">Task5</a></li>  <!--Task5-->
+    <li><a href="/Php-Task7/Php-Task6/index.php">Task6</a></li>  <!--Task6-->
+    <form action="login.php" method = "POST">
+    <input type="submit" value="Logout" name="logout">   <!--Logout option-->
+    </form>
+  </ul>
 </div>
 <?php
 session_start();
 if(isset($_POST["logout"])){
-    session_unset();  //unset the value
+  session_unset();  //unset the value
+  session_destroy();  // destroy the value
 }
 ?>
 
