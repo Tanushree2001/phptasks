ba34b9a565f1e62bfcf297d0a97ea932ab2e4970
tanushree gupta <tanushree.gupta@innoraft.com>
2023-03-03 12:23:04 +0530

    FT2023-72-Updated with the indent spaces,styles and comments

--- Php-Task6/index.php
+++ Php-Task6/index.php
@@ -9,7 +9,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
 <body>
-    <form  method="post" enctype="multipart/form-data"> <!--form started and added enctype to upload files, post can't upload files on server-->
+  <section class="container">
+    <form  method="post" enctype="multipart/form-data" class=form> <!--form started and added enctype to upload files, post can't upload files on server-->
     First Name: <input type="text" name="fname" class="fname" required pattern="[A-Za-z]*"><br> <!--First name created-->
     Last Name: <input type="text" name="lname" class="lname" required pattern="[A-Za-z]*"><br> <!--last name created-->
     Full Name: <input type="text" name="fullname" class="fullname" readonly> <br><!--fullname created-->
@@ -21,13 +22,14 @@
     <br>
     <input type="submit" class="submit"><br> <!--submit button--> 
   </form>  <!--Form closed-->
+  </section>
   <?php include 'welcome.php';?>
   <script> 
     $(document).ready(function () {
-            $(".fname, .lname").on('input',function() {  //whenever firstname and lastname filled, input should be done in fullname.
-                $(".fullname").val($('.fname').val() +" "+ $('.lname').val()); //fullname should appear with the concatenation of first name and last name.
-            });
-        });
+      $(".fname, .lname").on('input',function() {  //whenever firstname and lastname filled, input should be done in fullname.
+          $(".fullname").val($('.fname').val() +" "+ $('.lname').val()); //fullname should appear with the concatenation of first name and last name.
+      });
+  });
   </script>
 </body>
 </html>
