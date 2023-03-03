e8fe2bb2d2e51d6b64cdd867fce27e42e290cdda
tanushree gupta <tanushree.gupta@innoraft.com>
2023-03-03 09:46:29 +0530

    FT2023-72-Updated with the indent spaces and style

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
 <body>
+    <section class="container">
+      <div class="box">
     <form action="welcome.php" method="post"> <!--form started -->
     First Name: <input type="text" name="fname" class="fname" required pattern="[A-Za-z]*"><br> <!--First name created-->
     Last Name: <input type="text" name="lname" class="lname" required pattern="[A-Za-z]*"><br> <!--last name created-->
@@ -16,12 +19,14 @@
     <br>
     <input type="submit" class="submit"><br> <!--submit button--> 
   </form>  <!--Form closed-->
+</div>
+    </section>
   <script> 
     $(document).ready(function () {
-            $(".fname, .lname").on('input',function() {  //when click on submit button
-                $(".fullname").val($('.fname').val() +" "+ $('.lname').val()); //fullname should appear
-            });
+      $(".fname, .lname").on('input',function() {  //when input the value of firstname and lastname 
+          $(".fullname").val($('.fname').val() +" "+ $('.lname').val()); //fullname will appear with the conactenation of first name and last name
         });
+      });
   </script>
-</body>
+</body> 
 </html>
--- welcome.php
+++ welcome.php
@@ -1,38 +1,35 @@
 <html>
-    <body>
-        <?php
-        // class created by name forms
-        class Forms{
-            // initialize variables
-            public $first_name, $last_name, $img_name, $img_tmp;
+  <body>
+    <?php
+    // class created by name forms
+    class Forms{
+      // initialize variables
+      public $first_name, $last_name, $img_name, $img_tmp;
 
-            // constructor created 
-            public function __construct($firstname, $lastname)
-            {
-                $this->first_name = $firstname;
-                $this->last_name = $lastname;    
-                
-            }
+      // constructor created 
+      public function __construct($firstname, $lastname)
+      {
+        $this->first_name = $firstname;
+        $this->last_name = $lastname;    
+          
+      }
 
-            // created function to show full name 
-            public function FullName()
-            {
-                $full = "Welcome " . $this->first_name . " " . $this->last_name;
-                echo $full; //printing full name
-            }
-
-        }
-        
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $firstname = $_POST["fname"]; 
-            $lastname = $_POST["lname"]; 
-            $imgname=$_FILES['img_upload']['name']; //Use $_FILES for uploading image and from the array of folder we choose a key: name and store it in a variable.
-            $imgtmp=$_FILES['img_upload']['tmp_name']; // from array of folder we choose a key tmp_name and store it in a variable
-            $new = new Forms($firstname, $lastname ); //calling constructor
-            
-            $new->FullName(); //calling function Fullname
-        }
-        ?>
-        
-    </body>
+      // created function to show full name 
+      public function FullName()
+      {
+        $full = "Welcome " . $this->first_name . " " . $this->last_name;
+        echo $full; //printing full name
+      }
+    }
+    
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $firstname = $_POST["fname"]; 
+    $lastname = $_POST["lname"]; 
+    $new = new Forms($firstname, $lastname ); //calling constructor
+    
+    $new->FullName(); //calling function Fullname
+  }
+  ?>
+      
+  </body>
 </html>
