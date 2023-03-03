08c33ddc94c3d325a3eeb0c0096a47554069c8c8
tanushree gupta <tanushree.gupta@innoraft.com>
2023-03-03 11:57:30 +0530

    FT2023-72-Updated with the indent spaces,styles and comments

--- Php-Task2/index.php
+++ Php-Task2/index.php
@@ -1,29 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
 <body>
-    <form action="welcome.php" method="post" enctype="multipart/form-data"> <!--form started and added enctype to upload files, post can't upload files on server-->
-    First Name: <input type="text" name="fname" class="fname" required pattern="[A-Za-z]*"><br> <!--First name created-->
-    Last Name: <input type="text" name="lname" class="lname" required pattern="[A-Za-z]*"><br> <!--last name created-->
-    Full Name: <input type="text" name="fullname" class="fullname" readonly> <br><!--fullname created-->
-    Add Image: <input type="file" name="img_upload" class="upload" required accept=".jpg,.jpeg,.png"> <!--for adding file created input-->
-    <br>
-    <br>
-    <input type="submit" class="submit"><br> <!--submit button--> 
+  <section class="container">
+  <div class="form">
+  <form action="welcome.php" method="post" enctype="multipart/form-data"> <!--form started and added enctype to upload files, post can't upload files on server-->
+  First Name: <input type="text" name="fname" class="fname" required pattern="[A-Za-z]*"><br> <!--First name created-->
+  Last Name: <input type="text" name="lname" class="lname" required pattern="[A-Za-z]*"><br> <!--last name created-->
+  Full Name: <input type="text" name="fullname" class="fullname" readonly> <br><!--fullname created-->
+  Add Image: <input type="file" name="img_upload" class="upload" required accept=".jpg,.jpeg,.png"> <!--for adding file created input-->
+  <br>
+  <br>
+  <input type="submit" class="submit"><br> <!--submit button--> 
   </form>  <!--Form closed-->
+  </div>
+</section>
   <script> 
     $(document).ready(function () {
-            $(".fname, .lname").on('input',function() {  //when click on submit button
-                $(".fullname").val($('.fname').val() +" "+ $('.lname').val()); //fullname should appear
-            });
-        });
+      $(".fname, .lname").on('input',function() {  ////when input the value of firstname and lastname 
+        $(".fullname").val($('.fname').val() +" "+ $('.lname').val()); //fullname will appear with the conactenation of first name and last name
+      });
+    });
   </script>
 </body>
 </html>
--- Php-Task2/welcome.php
+++ Php-Task2/welcome.php
@@ -1,46 +1,45 @@
 <html>
-    <body>
-        <?php
-        // class created by name forms
-        class Forms{
-            // initialize variables
-            public $first_name, $last_name, $img_name, $img_tmp;
+<body>
+  <?php
+  // class created by name forms
+  class Forms{
+  // initialize variables
+  public $first_name, $last_name, $img_name, $img_tmp;
 
-            // constructor created 
-            public function __construct($firstname, $lastname, $imgname, $imgtmp)
-            {
-                $this->first_name = $firstname;
-                $this->last_name = $lastname;    
-                $this->img_name = $imgname;
-                $this->img_tmp = $imgtmp;
-            }
+  // constructor created 
+  public function __construct($firstname, $lastname, $imgname, $imgtmp)
+  {
+    $this->first_name = $firstname;
+    $this->last_name = $lastname;    
+    $this->img_name = $imgname;
+    $this->img_tmp = $imgtmp;
+  }
 
-            // created function to show full name 
-            public function FullName()
-            {
-                $full = "Welcome " . $this->first_name . " " . $this->last_name;
-                echo $full; //printing full name
-            }
+  // created function to show full name 
+  public function FullName()
+  {
+    $full = "Welcome " . $this->first_name . " " . $this->last_name;
+    echo $full; //printing full name
+  }
 
-            // created function to show Image
-            public function ShowImage()
-            {
-                move_uploaded_file($this->img_tmp ,"uploadimg/".$this->img_name); //php function for image upload contain tmp_name and destination of file.
-                echo "<p><img src='uploadimg/$this->img_name' alt='img'></p>";  //used image tag and gave the source of file and printed it.
-            }
-
-        }
-        
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $firstname = $_POST["fname"]; 
-            $lastname = $_POST["lname"]; 
-            $imgname=$_FILES['img_upload']['name']; //Use $_FILES for uploading image and from the array of folder we choose a key: name and store it in a variable.
-            $imgtmp=$_FILES['img_upload']['tmp_name']; // from array of folder we choose a key tmp_name and store it in a variable
-            $new = new Forms($firstname, $lastname, $imgname, $imgtmp); //calling constructor
-            $new->ShowImage(); // calling function ShowImage
-            $new->FullName(); //calling function Fullname
-        }
-        ?>
-        
-    </body>
+  // created function to show Image
+  public function ShowImage()
+  {
+    move_uploaded_file($this->img_tmp ,"uploadimg/".$this->img_name); //php function for image upload contain tmp_name and destination of file.
+    echo "<p><img src='uploadimg/$this->img_name' alt='img'></p>";  //used image tag and gave the source of file and printed it.
+  }
+}
+      
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $firstname = $_POST["fname"]; 
+    $lastname = $_POST["lname"]; 
+    $imgname=$_FILES['img_upload']['name']; //Use $_FILES for uploading image and from the array of folder we choose a key: name and store it in a variable.
+    $imgtmp=$_FILES['img_upload']['tmp_name']; // from array of folder we choose a key tmp_name and store it in a variable
+    $new = new Forms($firstname, $lastname, $imgname, $imgtmp); //calling constructor
+    $new->ShowImage(); // calling function ShowImage
+    $new->FullName(); //calling function Fullname
+  }
+?>
+      
+  </body>
 </html>
